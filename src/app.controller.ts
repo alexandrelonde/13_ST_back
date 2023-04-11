d4926d796c4bf379cdc11b3372fd32db6cb7eda2
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-10 21:19:11 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // Criando uma nova rota - como é uma rota get também, terá que ter outro nome
+  @Get('objeto')
+  getObject(): any {
+    return this.appService.getObject();
+    
+  }
+}
+
