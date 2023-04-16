e41e8315460400aa2c9a203b10bb380b67cb9a22
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-16 20:51:48 -0300

    feat: add route BUSCAR

--- src/restaurante/restaurante.service.ts
+++ src/restaurante/restaurante.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Restaurante } from './entities/restaurante.entity';
+import { SearchDto } from './dto/procuraRestaurante.dto';
 
 @Injectable()
 export class RestauranteService {
@@ -20,4 +21,29 @@ export class RestauranteService {
     .where('restaurante.id = :id', { id: id })
     .getOne();
   }
+
+  async search(searchDto: SearchDto): Promise<Restaurante[]> {
+    const { term } = searchDto;
+    const searchTerm = `%${term}%`.toLowerCase();
+  
+    try {
+      const restaurantesEncontrados = await this.restauranteRepository
+        .createQueryBuilder('restaurante')
+        .leftJoinAndSelect('restaurante.pratos', 'prato')
+        .where('LOWER(restaurante.nome) LIKE :searchTerm', { searchTerm })
+        .orWhere('LOWER(prato.nome) LIKE :searchTerm', { searchTerm })
+        .orWhere('LOWER(prato.descricao) LIKE :searchTerm', { searchTerm })
+        .getMany();
+
+      return restaurantesEncontrados;
+    } catch (error) {
+      throw new Error('Ocorreu um erro ao realizar a busca.');
+    }
+  }
 }
+
+
+
+
+
+
