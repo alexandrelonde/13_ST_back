1955b3b4a6fe656691cc65a87955ba1bf43a64f6
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-07-23 00:09:23 -0300

    fix: variáveis de ambiente

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,24 +5,24 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-      private usuarioService: UsuarioService,
-      private jwtService: JwtService
-    ) {}
+  constructor(
+    private usuarioService: UsuarioService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validarUsuario(email: string, senha: string): Promise<any> {
-      const usuario = await this.usuarioService.findOne(email);
-      if (usuario && bcrypt.compareSync(senha, usuario.password)) {
-        const { password, ...result } = usuario;
-        return result;
-      }
-      return null;
+  async validarUsuario(email: string, senha: string): Promise<any> {
+    const usuario = await this.usuarioService.findOne(email);
+    if (usuario && bcrypt.compareSync(senha, usuario.password)) {
+      const { password, ...result } = usuario;
+      return result;
     }
+    return null;
+  }
 
-    async login(user: any) {
-      const payload = { username: user.username, sub: user.userId };
-      return {
-        access_token: await this.jwtService.sign(payload),
-      };
-    }
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: await this.jwtService.sign(payload),
+    };
+  }
 }
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,4 +1,7 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const databaseProviders = [
   {
@@ -6,14 +9,12 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '12345',
-        database: 'sabortech',
-        entities: [
-            __dirname + '/../**/*.entity{.ts,.js}',
-        ],
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, // Sincroniza a criação do banco de dados a partir da entity
       });
 
